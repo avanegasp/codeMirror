8cf8f2d71de63846f8e60303a866919ecc22d16f
avanegasp <arco527@gmail.com>
2017-07-23 22:21:31 -0500

    Add files

--- js/default.js
+++ js/default.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   var code = $(".codemirror-textarea")[0];
   var editor = CodeMirror.fromTextArea(code, {
       lineNumbers : true,
-      theme: 'seti'
+      theme: 'seti',
+      value: "function myScript(){return 100;}\n",
+      mode:  "javascript"
   });
 });
